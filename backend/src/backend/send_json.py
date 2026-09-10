ac131ab6e64c28e037b66d98416e18af18d8c0ae
William Warwick <warwickw8@gmail.com>
2026-09-10 21:59:26 +0200

    CORS

--- backend/src/backend/send_json.py
+++ backend/src/backend/send_json.py
@@ -1,9 +1,17 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import ValidationError
 from .process_schedule import analyze_schedule
 from .models import ScheduleResponse
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:4200"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.post("/schedule", response_model=ScheduleResponse)
 def create_schedule():
